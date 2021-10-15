8f1a65de3b16bf9f21fdb8e06de5e5454a6935af
RukasuDesuu <lucascamargolucas@hotmail.com>
2021-10-15 10:09:10 -0300

    Starting Arduino Comms

--- ShedPy/main.py
+++ ShedPy/main.py
@@ -1,24 +1,21 @@
-#!/usr/bin/env python
-# pylint: disable=C0116,W0613
-# This program is dedicated to the public domain under the CC0 license.
-
-"""
-Simple Bot to reply to Telegram messages.
-First, a few handler functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
 from functools import update_wrapper
 import logging
 
+#libraries for telegram bot
 #pip3 install python-telegram-bot
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+#libraries for arduino comms
+import threading
+import serial
+import time
+
+conectado = False #variável booleana para condição de conexão do arduino
+porta = 'COM3' #Variável para porta serial do arduino
+velocidadeBaud = 9600 #Variável para velocidade de envio na porta serial
+
+
 
 # Enable logging
 logging.basicConfig(
@@ -27,12 +24,18 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-IDArchive = open("ids.txt", "r")
-permID = IDArchive.read().splitlines()
-IDArchive.close()
-print(permID)
-
 chaveObj = ["cortina", "led", "luz"]
+
+def TeleTokens(path, shoudSplit):
+    archive = open(path, "r+")
+    value = archive.read()
+    if shoudSplit:
+        value.splitlines()
+    archive.close()
+    return(value)
+
+token = TeleTokens("token.txt", 0)
+permID = TeleTokens("ids.txt", 1)   
 
 
 def trust(id):
@@ -44,6 +47,7 @@ def trust(id):
 def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
     id = update.effective_user.id
+    print(id)
     if trust(id):
         user = update.effective_user
         update.message.reply_markdown_v2(
@@ -62,8 +66,9 @@ def help_command(update: Update, context: CallbackContext) -> None:
         update.message.reply_text("Acesso Negado!!")
 
 
-def auto(update: Update, context: CallbackContext) -> None:
+def signal(update: Update, context: CallbackContext) -> None:
     id = update.effective_user.id
+    print(id)
     msg = update.message.text.lower()
     if trust(id):
         if "abrir" in msg or "ligar" in msg and not "desligar" in msg:
@@ -80,20 +85,17 @@ def auto(update: Update, context: CallbackContext) -> None:
                 break
         if not Bobj:
             update.message.reply_text("Erro!\nObjeto não declarado ou nao disponivel :(")
+
         update.message.reply_text(cond +" "+ obj)
     else:
         update.message.reply_text("Acesso Negado!!")
-
+        
 
 def main() -> None:
     
     """Start the bot."""
     # Create the Updater and pass it your bot's token.
-    tokenArchive = open("token.txt")
-    token = tokenArchive.read()
-    tokenArchive.close()
     updater = Updater(token)
-
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
@@ -103,8 +105,8 @@ def main() -> None:
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
 
-    # on non command i.e message - run function auto
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, auto))
+    # on non command i.e message - run function signal
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, signal))
 
     # Start the Bot
     updater.start_polling()
